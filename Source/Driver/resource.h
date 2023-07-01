efda63dccac43e6a3a4ae3bbad4d71657a61e34e
박현우 <phu54321@naver.com>
2023-07-01 16:52:47 +0900

    cleanup resources

--- Source/Driver/resource.h
+++ Source/Driver/resource.h
@@ -1,20 +1,2 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by ASIO2WASAPI2.rc
-//
-#define IDD_CONTROL_PANEL 7
 #define IDI_ICON1 101
-#define IDL_DEVICE 1001
-#define IDC_CHANNELS 1002
-#define IDC_SAMPLE_RATE 1006
 
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE 102
-#define _APS_NEXT_COMMAND_VALUE 40001
-#define _APS_NEXT_CONTROL_VALUE 1007
-#define _APS_NEXT_SYMED_VALUE 101
-#endif
-#endif
